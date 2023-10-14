cb092537b27717df9c82bd762136b02d6bdf0aaa
Dil Khan <dilkhan8989@gmail.com>
2023-10-14 17:35:19 +0300

    Add bg image & update style

--- script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', function () {
-    var nav = document.querySelector('nav');
-    if (window.scrollY > 100) { 
-        nav.style.backgroundColor = '#ff9900'; 
-    } else {
-        nav.style.backgroundColor = '#333'; 
-    }
-});
